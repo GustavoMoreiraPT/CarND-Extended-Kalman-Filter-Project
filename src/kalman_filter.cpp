4e7c260d74a6744609acacaeb7f0fd40cd643a52
Gustavo <gustavo1995.2012@hotmail.com>
2020-06-28 17:04:46 +0100

    Fixed implementation typos across the kalman_filter.cpp class

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -23,9 +23,9 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  this->x_ = this->F_ _ this->x_;
+  this->x_ = this->F_ * this->x_;
   MatrixXd Ft = this->F_.transpose();
-  this->P = this->F_ * this->P_ * Ft + this->Q_; 
+  this->P_ = this->F_ * this->P_ * Ft + this->Q_; 
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -34,14 +34,14 @@ void KalmanFilter::Update(const VectorXd &z) {
   MatrixXd Ht = this->H_.transpose();
   MatrixXd S = this->H_ * this->P_ * Ht * this->R_;
   MatrixXd Si = S.inverse();
-  MatrixXd PHt = this->P * Ht;
+  MatrixXd PHt = this->P_ * Ht;
   MatrixXd K = PHt * Si;
 
   //here we do the new estimation by updtating the values of matrix P and vector x_
   this->x_ = this->x_ + (K * y);
-  long x_size = this.x_.size();
+  long x_size = this->x_.size();
   MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  this->P_ = (I - k * this->H_) * this->P_;
+  this->P_ = (I - K * this->H_) * this->P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -53,8 +53,8 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
     float vy = this->x_[3];
 
     //first conversion in the vector
-    float rho = sqrt(px*px + py*py)
-    float phi = atan2(py, px)
+    float rho = sqrt(px*px + py*py);
+    float phi = atan2(py, px);
     float rho_dot = 0;
 
     if(fabs(rho) > 0.001){
@@ -75,7 +75,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
 
     //here we do the new estimation by updtating the values of matrix P and vector x_
     this->x_ = this->x_ + (K * y);
-    long x_size = this.x_.size();
+    long x_size = this->x_.size();
     MatrixXd I = MatrixXd::Identity(x_size, x_size);
-    this->P_ = (I - k * this->H_) * this->P_;
+    this->P_ = (I - K * this->H_) * this->P_;
 }
