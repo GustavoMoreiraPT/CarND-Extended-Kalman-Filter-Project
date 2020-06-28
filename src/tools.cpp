ac1899dda0ae1529c7de34f8fcf91c29dc639e5d
Gustavo <gustavo1995.2012@hotmail.com>
2020-06-28 18:06:33 +0100

    optimization

--- src/tools.cpp
+++ src/tools.cpp
@@ -29,7 +29,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     VectorXd residual = estimations[i] - ground_truth[i];
 
     residual = residual.array()*residual.array();
-    rmse = rmse + residual;
+    rmse += residual;
  }
 
  //calculate the mean
